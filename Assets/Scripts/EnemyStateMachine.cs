fb4d269e3f7e174bbdecc832ac20317980753903
Rheza-Rivaldi <101277746+Rheza-Rivaldi@users.noreply.github.com>
2022-03-20 00:39:27 +0700

    Add StateMachine and Progressbar
    Added enum states and activate progressbar on correct state

--- Assets/Scripts/EnemyStateMachine.cs
+++ Assets/Scripts/EnemyStateMachine.cs
@@ -7,15 +7,59 @@ public class EnemyStateMachine : MonoBehaviour
 
     public BaseClass enemy;
 
-    // Start is called before the first frame update
+    public enum TurnState
+    {
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD
+    }
+    public TurnState currentState;
+
+    //for progressbar
+    float cur_cooldown = 0f;
+    float max_cooldown = 3.5f;
+
+
     void Start()
     {
-        
+        currentState = TurnState.PROCESSING;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case(TurnState.PROCESSING):
+            UpdateProgressBar();
+            break;
+            
+            case(TurnState.ADDTOLIST):
+            break;
+
+            case(TurnState.WAITING):
+            break;
+
+            case(TurnState.SELECTING):
+            break;
+
+            case(TurnState.ACTION):
+            break;
+
+            case(TurnState.DEAD):
+            break;
+        }
+    }
+
+     void UpdateProgressBar()
+    {
+        cur_cooldown = cur_cooldown + Time.deltaTime;
+
+        if (cur_cooldown >= max_cooldown){
+            currentState = TurnState.ADDTOLIST;
+        }
     }
 }
--- Assets/Scripts/HeroStateMachine.cs
+++ Assets/Scripts/HeroStateMachine.cs
@@ -1,21 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroStateMachine : MonoBehaviour
 {
 
     public BaseClass hero;
-    
-    // Start is called before the first frame update
+
+    public enum TurnState
+    {
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD
+    }
+    public TurnState currentState;
+
+    //for progressbar
+    float cur_cooldown = 0f;
+    float max_cooldown = 3f;
+    public Image ProgressBar;
+
+
     void Start()
     {
-        
+        currentState = TurnState.PROCESSING;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        //Debug.Log (currentState);
+        switch (currentState)
+        {
+            case(TurnState.PROCESSING):
+            UpdateProgressBar();
+            break;
+            
+            case(TurnState.ADDTOLIST):
+            break;
+
+            case(TurnState.WAITING):
+            break;
+
+            case(TurnState.SELECTING):
+            break;
+
+            case(TurnState.ACTION):
+            break;
+
+            case(TurnState.DEAD):
+            break;
+        }
+    }
+
+    void UpdateProgressBar()
+    {
+        cur_cooldown = cur_cooldown + Time.deltaTime;
+        float calc_cooldown = cur_cooldown / max_cooldown;
+        ProgressBar.transform.localScale = new Vector2 (Mathf.Clamp(calc_cooldown,0,1), ProgressBar.transform.localScale.y);
+
+        if (cur_cooldown >= max_cooldown){
+            currentState = TurnState.ADDTOLIST;
+        }
     }
 }
